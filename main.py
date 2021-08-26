69a10bbb999ab95c8e476d19fc9ad56977266daf
Sam Kenney <sam.kenney@louder.com.au>
2021-08-26 19:05:39 +1000

    optimising main code

--- main.py
+++ main.py
@@ -11,82 +11,124 @@ ARDUINO = Serial(port=PORT, baudrate=115200, timeout=0.1)
 CASCADE_PATH = os.path.join("resources", "haarcascade_frontalface_default.xml")
 CASCADE = cv2.CascadeClassifier(CASCADE_PATH)
 
-cap = cv2.VideoCapture(0)
-print("Accessed camera")
 
-cap.set(3, 1920)
-print("Width set")
+def main():
+    cap = cv2.VideoCapture(0)
+    print("Accessed camera")
 
-cap.set(4, 1080)
-print("Height set")
+    cap.set(3, 1920)
+    print("Width set")
 
-print("Camera enabled")
+    cap.set(4, 1080)
+    print("Height set")
 
-while True:
-    success, img = cap.read()
-    (ch, cw) = img.shape[:2]
-    center_height = ch // 2
-    center_width = cw // 2
-    circ = cv2.circle(
-        img=img,
-        center=(center_width, center_height),
-        radius=7,
-        color=(255, 255, 255),
-        thickness=-1,
-    )
+    print("Camera enabled")
 
-    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = CASCADE.detectMultiScale(gray_img, 1.1, 4)
-
-    for (x, y, w, h) in faces:
-        rect = cv2.rectangle(
+    while True:
+        success, img = cap.read()
+        (ch, cw) = img.shape[:2]
+        center_height = ch // 2
+        center_width = cw // 2
+        circ = cv2.circle(
             img=img,
-            pt1=(x, y),
-            pt2=(x + w, y + h),
-            color=(0, 0, 255),
-            thickness=2,
-        )
-
-        rect_center_x = (x + (x + w)) // 2
-        rect_center_y = (y + (y + h)) // 2
-
-        rect_center = cv2.circle(
-            img=img,
-            center=(rect_center_x, rect_center_y),
+            center=(center_width, center_height),
             radius=7,
-            color=(0, 0, 255),
+            color=(255, 255, 255),
             thickness=-1,
         )
 
-        if int(rect_center_y) >= int(center_height) + OFFSET:
-            print("Too low")
-            ARDUINO.write(bytes("1", "utf-8"))
+        gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = CASCADE.detectMultiScale(gray_img, 1.1, 4)
 
-        elif int(rect_center_y) <= int(center_height) - OFFSET:
-            print("Too high")
-            ARDUINO.write(bytes("2", "utf-8"))
+        for (x, y, w, h) in faces:
+            rect = cv2.rectangle(
+                img=img,
+                pt1=(x, y),
+                pt2=(x + w, y + h),
+                color=(0, 0, 255),
+                thickness=2,
+            )
 
-        else:
-            print("Height centered")
-            ARDUINO.write(bytes("0", "utf-8"))
-        time.sleep(0.05)
+            rect_center_x = (x + (x + w)) // 2
+            rect_center_y = (y + (y + h)) // 2
 
-        if int(rect_center_x) >= int(center_width) + OFFSET:
-            print("Too far left")
-            ARDUINO.write(bytes("3", "utf-8"))
+            rect_center = cv2.circle(
+                img=img,
+                center=(rect_center_x, rect_center_y),
+                radius=7,
+                color=(0, 0, 255),
+                thickness=-1,
+            )
 
-        if int(rect_center_x) <= int(center_width) - OFFSET:
-            print("Too far right")
-            ARDUINO.write(bytes("4", "utf-8"))
+            direct_aurdino(
+                rectangle_x=rect_center_x,
+                rectangle_y=rect_center_y,
+                center_height=center_height,
+                center_width=center_width,
+            )
 
-        else:
-            print("Width centered")
-            ARDUINO.write(bytes("0", "utf-8"))
-        time.sleep(0.05)
+        cv2.imshow("Faces", img)
 
-    cv2.imshow("Faces", img)
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
 
-    if cv2.waitKey(1) & 0xFF == ord("q"):
-        break
+    cv2.destroyAllWindows()
 
-cv2.destroyAllWindows()
+
+def write(x):
+    ARDUINO.write(bytes(str(x), "utf-8"))
+    # time.sleep(0.05)
+
+
+def direct_aurdino(rectangle_x, rectangle_y, center_height, center_width):
+    if (
+        int(rectangle_y) >= int(center_height) + OFFSET
+        and int(rectangle_x) >= int(center_width) + OFFSET
+    ):
+        print("Too low and too far left")
+        write("1")
+
+    elif (
+        int(rectangle_y) >= int(center_height) + OFFSET
+        and int(rectangle_x) <= int(center_width) - OFFSET
+    ):
+        print("Too low and too far right")
+        write("2")
+
+    elif (
+        int(rectangle_y) <= int(center_height) - OFFSET
+        and int(rectangle_x) >= int(center_width) + OFFSET
+    ):
+        print("Too high and too far left")
+        write("3")
+
+    elif (
+        int(rectangle_y) <= int(center_height) - OFFSET
+        and int(rectangle_x) <= int(center_width) - OFFSET
+    ):
+        print("Too high and too far right")
+        write("4")
+
+    elif int(rectangle_y) >= int(center_height) + OFFSET:
+        print("Too low")
+        write("5")
+
+    elif int(rectangle_y) <= int(center_height) - OFFSET:
+        print("Too high")
+        write("6")
+
+    elif int(rectangle_x) >= int(center_width) + OFFSET:
+        print("Too far left")
+        write("7")
+
+    elif int(rectangle_x) <= int(center_width) - OFFSET:
+        print("Too far right")
+        write("8")
+
+    else:
+        print("Centered")
+        write("0")
+
+
+if __name__ == "__main__":
+    main()
